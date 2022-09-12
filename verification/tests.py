78fe1a3efe92db261aee6704cb6cf70ea1eb8272
Alexander Blaschuk <blaschuk95@gmail.com>
2022-09-12 16:30:49 +0300

    mission-create

---
+++ verification/tests.py
@@ -0,0 +1,59 @@
+"""
+TESTS is a dict with all you tests.
+Keys for this will be categories' names.
+Each test is dict with
+    "input" -- input data for user function
+    "answer" -- your right answer
+    "explanation" -- not necessary key, it's using for additional info in animation.
+"""
+
+TESTS = {
+    "Basics": [
+        {
+            "input": ["btry", "byrt"],
+            "answer": True
+        },
+        {
+            "input": ["boss", "boss"],
+            "answer": True
+        },
+        {
+            "input": ["solid", "disel"],
+            "answer": False
+        },
+        {
+            "input": ["false", "flaes"],
+            "answer": False
+        },
+        {
+            "input": ["true", "treu"],
+            "answer": True
+        }
+    ],
+    "Extra": [
+        {
+            "input": ["race", "care"],
+            "answer": True
+        },
+        {
+            "input": ["knee", "keen"],
+            "answer": True
+        },
+        {
+            "input": ["trap", "part"],
+            "answer": False
+        },
+        {
+            "input": ["adkglaigrbosudfbklabsdlkgalkkfndah", "adkglaigrbosudfbllabsdlkgakkkfndah"],
+            "answer": True
+        },
+        {
+            "input": ["adkglaigrbosudfbklabsdlkgalkkfndah", "adkglaogrbisudlbkfabsdlkgalkkfndah"],
+            "answer": False
+        },
+        {
+            "input": ["abracadabra", "badaboom"],
+            "answer": False
+        }
+    ]
+}
